e128059d1e20b3dd67b2e29aeeeb3ed630cb8214
Daria Kafler <me@dariakafler.com>
2023-09-17 15:25:59 +0100

    Adding initial data flow parsing in management command

--- octopchallange/management/commands/importflow.py
+++ octopchallange/management/commands/importflow.py
@@ -7,4 +7,21 @@ class Command(BaseCommand):
     parser.add_argument('filepath', type=str, help='data flow file path')
 
   def handle(self, *args, **options):
-    print(f'Importing file: {options["filepath"]}')
+    filepath = options["filepath"]
+    print(f'Importing file: {filepath}')
+    
+    file = open(filepath, 'r')
+    lines = file.readlines()
+
+    count = 0
+    for line in lines:
+      fields = line.split("|")
+      type = fields[0]
+      if type == '026':      
+        mpan = fields[1]
+      if type == '028':        
+        meter_id = fields[1]
+      if type == '030':
+        print(f'MPAN: {mpan}, Meter ID: {meter_id}, Meter Register: {fields[1]}, DateTime: {fields[2]}, Reading: {fields[3]}')
+        count+=1
+    print(f'Total Readings: {count}')
