361681165d6c65ae5b773b0b04c8894a04023e3c
Daria Kafler <me@dariakafler.com>
2023-09-17 14:55:53 +0100

    Adding initial Reading model (might change later)

--- octopchallange/models.py
+++ octopchallange/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Reading(models.Model):
+	mpan = models.IntegerField(max_length=13)
+	meter_id = models.CharField(max_length=10)
+	meter_register = models.CharField(max_length=2)
+	reading_date_time = models.DateTimeField()
+	register_reading = models.DecimalField(max_digits=10)
