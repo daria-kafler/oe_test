d174068e4b17acff672b8883eee0b60882c181de
Daria Kafler <me@dariakafler.com>
2023-09-17 16:00:55 +0100

    Add Reading model to admin panel

--- octopchallange/admin.py
+++ octopchallange/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from octopchallange.models import Reading
+
 # Register your models here.
+class ReadingAdmin(admin.ModelAdmin):
+    list_display = ('mpan', 'meter_id', 'meter_register', 'reading_date_time', 'register_reading')
+    search_fields = ('mpan', 'meter_id')
+    
+admin.site.register(Reading, ReadingAdmin)
