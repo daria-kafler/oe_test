74f3c0a1f7c3658f8404a1a4d4008939d31e5a64
Daria Kafler <me@dariakafler.com>
2023-09-17 16:00:24 +0100

    Add actual db import to managment command

--- octopchallange/management/commands/importflow.py
+++ octopchallange/management/commands/importflow.py
@@ -1,4 +1,7 @@
+import decimal
+from time import strftime, strptime
 from django.core.management.base import BaseCommand, CommandError
+from octopchallange.models import Reading
 
 class Command(BaseCommand):
   help = "Import data flow file to the system"
@@ -22,6 +25,13 @@ class Command(BaseCommand):
       if type == '028':        
         meter_id = fields[1]
       if type == '030':
-        print(f'MPAN: {mpan}, Meter ID: {meter_id}, Meter Register: {fields[1]}, DateTime: {fields[2]}, Reading: {fields[3]}')
+        meter_register=fields[1]
+        reading_date_time=strftime('%Y-%m-%d %H:%M:%S', strptime(fields[2], '%Y%m%d%H%M%S'))
+        register_reading=decimal.Decimal(fields[3])
+
+        print(f'MPAN: {mpan}, Meter ID: {meter_id}, Meter Register: {meter_register}, DateTime: {reading_date_time}, Reading: {register_reading}')
+        reading = Reading(mpan=int(mpan), meter_id=meter_id, meter_register=meter_register, reading_date_time=reading_date_time, register_reading=register_reading)
+        reading.save()
+        print(f'Reading: {reading}')
         count+=1
     print(f'Total Readings: {count}')
